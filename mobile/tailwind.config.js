4bfeab4a060b370113a8130cfaa7b978b052e334
carrotjc <jerlyncardinas23@gmail.com>
2026-09-09 13:50:43 +0800

    chore: nativewind setup done

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{tsx,ts,jsx,js}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
